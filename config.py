afd7b5542283396cd58cf9a29801a4ab3bc8ac13
dilmax11 <91269158+dilmax11@users.noreply.github.com>
2026-07-04 12:20:53 +0500

    Update MySQL configuration for production environment

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
     DEBUG      = os.environ.get("FLASK_DEBUG", "0") == "1"
 
-    MYSQL_HOST     = os.environ.get("MYSQL_HOST", "localhost")
-    MYSQL_PORT     = int(os.environ.get("MYSQL_PORT", "3306"))
-    MYSQL_USER     = os.environ.get("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "")
-    MYSQL_DB       = os.environ.get("MYSQL_DB", "bilimdonlar")
-    MYSQL_SSL      = os.environ.get("MYSQL_SSL", "0") == "1"
+    MYSQL_HOST     = os.environ.get("MYSQL_HOST", "mysql-bdca485-bilimdonlar.h.aivencloud.com")
+    MYSQL_PORT     = int(os.environ.get("MYSQL_PORT", "25344"))
+    MYSQL_USER     = os.environ.get("MYSQL_USER", "avnadmin")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "AVNS_1q4D1XvJArgJ_fDwIHD")
+    MYSQL_DB       = os.environ.get("MYSQL_DB", "defaultdb")
+    MYSQL_SSL      = os.environ.get("MYSQL_SSL", "REQUIRED") == "1"
 
     TELEGRAM_BOT_TOKEN    = os.environ.get("TELEGRAM_BOT_TOKEN", "")
     TELEGRAM_BOT_USERNAME = os.environ.get("TELEGRAM_BOT_USERNAME", "")
